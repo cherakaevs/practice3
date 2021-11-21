2d65244a4f770ffc5d25ab88423c2e63a53b6fe2
karmikfeels <marble462@gmail.com>
2021-11-21 11:46:17 +0400

    practice 8 commit

---
+++ practice3/src/buildings/net/client/SerialClient.java
@@ -0,0 +1,74 @@
+package buildings.net.client;
+
+import buildings.*;
+import buildings.exceptions.BuildingUnderArrestException;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class SerialClient {
+    public static void main(String[] args) {
+        try{
+            Socket socket = new Socket("127.0.0.1", 0);
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+            Building building;
+            String type;
+            String result;
+
+            String path = "serialResult.txt";
+
+            String buildingsInfo = "buildings.txt";
+            String buildingsTypes = "types.txt";
+
+            FileWriter writer = new FileWriter(new File(path));
+            Scanner scanner = new Scanner(new FileReader(buildingsTypes));
+            BufferedReader reader = new BufferedReader(new FileReader(buildingsInfo));
+
+            Object price;
+
+            while (scanner.hasNext()){
+                type = scanner.nextLine();
+                System.out.println(type);
+
+                switch (type) {
+                    case "Dwelling":
+                        Buildings.setBuildingFactory(new DwellingFactory());
+                        break;
+                    case "OfficeBuilding":
+                        Buildings.setBuildingFactory(new OfficeFactory());
+                        break;
+                    case "Hotel":
+                        Buildings.setBuildingFactory(new HotelFactory());
+                        break;
+                }
+
+                dos.writeInt(1);
+                building = Buildings.readBuilding(reader);
+                System.out.println(building);
+                Buildings.serializeBuilding(building, dos);
+
+                price = new ObjectInputStream(dis).readObject();
+
+                if(price instanceof Double){
+                    System.out.println("\nPrice: " + price + "\n");
+                    writer.write(price + "\n");
+                }
+
+                if (price instanceof BuildingUnderArrestException){
+                    System.out.println("Building is arrested!\n");
+                    writer.write("Building is arrested!\n");
+                }
+            }
+
+            dos.writeInt(0);
+            writer.close();
+            dos.flush();
+        }
+        catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ practice3/src/buildings/net/server/parallel/BinaryServer.java
@@ -0,0 +1,111 @@
+package buildings.net.server.parallel;
+
+import buildings.*;
+import buildings.dwelling.Dwelling;
+import buildings.exceptions.BuildingUnderArrestException;
+import buildings.office.OfficeBuilding;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BinaryServer {
+
+    public static double priceCheck(Building building) throws BuildingUnderArrestException {
+        if (arestedBuildong(building))
+            throw new BuildingUnderArrestException();
+
+        double squarePrice;
+        int square = building.getSumSquare();
+        if (building instanceof Dwelling) {
+            squarePrice = 1000;
+        } else if (building instanceof OfficeBuilding) {
+            squarePrice = 1500;
+        } else {
+            squarePrice = 2000;
+        }
+
+        double result = square * squarePrice;
+        return result;
+    }
+
+    public static boolean arestedBuildong(Building building) {
+
+        int chance = (int) (Math.random() * 10);
+        if (chance > 8) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+
+        try (ServerSocket server = new ServerSocket(0)) {
+
+            System.out.println("Server started");
+
+            while (true) {
+                Socket socket = server.accept();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+
+                        try (
+                                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                        ) {
+                            String type;
+                            Building building;
+                            Double price;
+                            String result;
+
+                            while (!(type = dis.readUTF()).equals("Exit")) {
+                                System.out.println("\nBuilding type: " + type + "\n");
+
+                                switch (type) {
+                                    case "Dwelling":
+                                        Buildings.setBuildingFactory(new DwellingFactory());
+                                        break;
+                                    case "OfficeBuilding":
+                                        Buildings.setBuildingFactory(new OfficeFactory());
+                                        break;
+
+                                }
+
+                                building = Buildings.inputBuilding(dis);
+
+                                try {
+                                    System.out.println("Building building: " + building);
+                                    price = priceCheck(building);
+                                    System.out.println("Price: " + price);
+                                    result = price.toString();
+                                } catch (BuildingUnderArrestException e) {
+                                    result = "Building is arrested!";
+                                }
+
+                                dos.writeUTF(result);
+                                dos.flush();
+                                System.out.println("\n---------------------------------------------------------------------------");
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                socket.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
